a503f9d237f3cc20bf0e9484c959f514dd5e239c
vanshkalra2255 <vanshkalra2255@gmail.com>
2022-12-26 22:20:16 +0530

    Basic Feature or Funcationality

--- Basic Operation.py
+++ Basic Operation.py
@@ -2,3 +2,4 @@ print("Hello 's Everyone")
 print("d:Training\new\python")              # here due to spl sequence \n is acting as a escape sequence so it is creating a new line
 
 print(print("d:Training\\new\python"))          # so we can add \\ {forward slash} to cancel its effect
+
